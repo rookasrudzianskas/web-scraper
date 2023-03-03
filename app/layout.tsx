89375697cc877737a644a094ba726d078c5d5e0b
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 14:44:29 +0200

    feat: add header
    This commit adds a header to the layout.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import '../styles/globals.css'
 import Sidebar from "../components/Sidebar";
+import Header from "../components/Header";
 
 export const metadata = {
   title: 'Amazon Web Scraper',
@@ -10,10 +11,10 @@ export const metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="flex">
+      <body className="flex bg-[#f7fbff] h-screen">
         <Sidebar />
-        <main>
-          {/* Header */}
+        <main className="p-10 max-w-7xl w-full mx-auto">
+           <Header />
           {children}
         </main>
       </body>
