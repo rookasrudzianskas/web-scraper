8854899157403bfc4d95821dc3a5c17509a68887
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 18:41:06 +0200

    Add try/catch to activateScraper
    This is to prevent the API from crashing if there is an error.

--- pages/api/activateScraper.ts
+++ pages/api/activateScraper.ts
@@ -12,36 +12,43 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>
 ) {
+  try {
+    const {search} = req.body;
 
-  const {search} = req.body;
+    console.log("SEARCH", search);
 
-  console.log("SEARCH", search);
+    const response = await fetch(`https://api.brightdata.com/dca/trigger?collector=c_lesjv33b2l46cgnd1x&queue_next=1`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${process.env.BRIGHTDATA_API_KEY}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        search,
+      })
+    });
 
-  const response = await fetch(`https://api.brightdata.com/dca/trigger?collector=c_lesjv33b2l46cgnd1x&queue_next=1`, {
-    method: 'POST',
-    headers: {
-      Authorization: `Bearer ${process.env.BRIGHTDATA_API_KEY}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
+    const data = await response.json();
+    console.log('THE DATA IS', data)
+
+    const { collection_id, start_eta } = data;
+
+    await adminDb.collection('searches').doc(collection_id).set({
       search,
+      start_eta,
+      status: 'pending',
+      createdAt: admin.firestore.Timestamp.now(),
     })
-  });
 
-  const data = await response.json();
-  console.log('THE DATA IS', data)
-
-  const { collection_id, start_eta } = data;
-
-  await adminDb.collection('searches').doc(collection_id).set({
-    search,
-    start_eta,
-    status: 'pending',
-    createdAt: admin.firestore.Timestamp.now(),
-  })
-
-  res.status(200).json({
-    collection_id,
-    start_eta,
-  })
+    res.status(200).json({
+      collection_id,
+      start_eta,
+    })
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({
+      // @ts-ignore
+      error: e.message,
+    });
+  }
 }
