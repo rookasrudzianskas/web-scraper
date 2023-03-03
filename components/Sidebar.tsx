793624d68147fa17a59436bfd30694ddee944105
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 14:37:53 +0200

    feat: add magnifying glass icon
    - Add magnifying glass icon to the sidebar
    - Add title and subtitle to the sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {DocumentMagnifyingGlassIcon} from "@heroicons/react/24/outline";
 
 const Sidebar = ({}) => {
   return (
     <div>
-      
+      <div className="flex flex-col items-center justify-center mb-10">
+        <DocumentMagnifyingGlassIcon className="h-16 md:w-16 text-indigo-600" />
+        <h1 className="hidden md:inline-flex text-3xl mt-2">Web Scraper</h1>
+        <h2 className="hidden md:inline-flex text-xs italic">Scraping the Unscramble</h2>
+      </div>
+      <ul>
+
+      </ul>
     </div>
   );
 };
