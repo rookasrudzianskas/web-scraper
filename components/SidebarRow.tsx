ec754db553c5a494cef8cc9519f8d21636566ccf
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 20:04:18 +0200

    feat: Add a spinner to the sidebar
    This commit adds a spinner to the sidebar to indicate that the search is still
    being processed.
    
    This commit also adds a checkmark to the sidebar to indicate that the search has
    been processed.

--- components/SidebarRow.tsx
+++ components/SidebarRow.tsx
@@ -2,6 +2,8 @@
 import { DocumentData } from 'firebase/firestore';
 import React, {useEffect, useState} from 'react';
 import {usePathname, useRouter} from "next/navigation";
+import {CheckCircleIcon} from "@heroicons/react/24/outline";
+import Spinner from "react-spinkit";
 
 type Props = {
   doc: DocumentData
@@ -15,13 +17,23 @@ const SidebarRow = ({doc}: Props) => {
   useEffect(() => {
     if(!pathname) return;
     setActive(pathname.includes(doc.id));
-  }, [])
+  }, [pathname, doc])
 
   return (
-    <li className="" onClick={() => router.push(`/search/${doc.id}`)}>
-      <div>
+    <li className={`flex justify-between p-4 cursor-pointer hover:bg-white hover:shadow-md rounded-lg ${active && "bg-white shadow-md"}`} onClick={() => router.push(`/search/${doc.id}`)}>
+      <div className="flex flex-col justify-center">
         <p className="text-xs md:text-base font-bold">{doc.data().search}</p>
+        {doc.data().status === "pending" && (
+          <p className="text-xs text-gray-500">Scraping Information...</p>
+        )}
       </div>
+      <span className="ml-2">
+        {doc.data().status === 'pending' ? (
+          <Spinner name="cube-grid" fadeIn="none" color="indigo" />
+        ) : (
+          <CheckCircleIcon className="h-6 w-6 text-green-500" />
+        )}
+      </span>
     </li>
   );
 };
