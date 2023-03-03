1f8141ca3a07cb945a00af1059ab4bae86579fa4
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 14:31:37 +0200

    feat: add link to byrookas.com
    This commit adds a link to byrookas.com to the home page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline'
+import Link from "next/link";
 
 const HomePage = ({}) => {
   return (
-    <div>
-      <div>
-        <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
-      </div>
+    <div className="flex flex-col items-center justify-center">
+      <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
+      <h1 className="text-3xl text-center mt-2 text-black font-bold mb-5">Welcome to the Amazon Web Scraper</h1>
+      <h2 className="text-lg italic text-black/50 text-center">
+        Go ahead and search for a product on Amazon and we will scrape the data for you.
+      </h2>
+      <Link href="https://byrookas.com" className="text-lg text-center italic text-black/50">
+        https://www.byrookas.com
+      </Link>
     </div>
   );
 };
