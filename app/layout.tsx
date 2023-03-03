5dfcc64d9874a8f4763661a1b3a0d9333df343f6
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 14:42:22 +0200

    Add flexbox to layout
    This commit adds flexbox to the layout to make it more responsive.

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className="flex">
         <Sidebar />
         <main>
           {/* Header */}
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,11 +3,11 @@ import {DocumentMagnifyingGlassIcon} from "@heroicons/react/24/outline";
 
 const Sidebar = ({}) => {
   return (
-    <div>
+    <div className="p-2 md:p-10 py-6 overflow-y-auto border-b border-indigo-500/50">
       <div className="flex flex-col items-center justify-center mb-10">
         <DocumentMagnifyingGlassIcon className="h-16 md:w-16 text-indigo-600" />
-        <h1 className="hidden md:inline-flex text-3xl mt-2">Web Scraper</h1>
-        <h2 className="hidden md:inline-flex text-xs italic">Scraping the Unscramble</h2>
+        <h1 className="hidden md:inline text-center text-3xl mt-2">Web Scraper</h1>
+        <h2 className="hidden md:inline text-center text-xs italic">Scraping the Unscramble</h2>
       </div>
       <ul>
 
