0cd613014bcec8376720fcaff75394c21a7d018c
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 15:05:04 +0200

    Add search to activateScraper route
    This is a test commit message

--- app/activateScraper/route.ts
+++ app/activateScraper/route.ts
@@ -1,8 +1,11 @@
 export async function POST(req: Request) {
-
+  const search = req.body?.search;
 }
 
 export async function GET(req: Request) {
+
+
+
   return new Response('Hello Rokas', {
     status: 200,
   });
