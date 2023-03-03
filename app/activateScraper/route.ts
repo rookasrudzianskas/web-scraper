381d2784e86d0589008d5ab456c99c686e28c24e
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 18:27:46 +0200

    Add logging to activateScraper route
    This will help us debug the issue with the scraper not activating.

--- app/activateScraper/route.ts
+++ app/activateScraper/route.ts
@@ -1,5 +1,8 @@
 import {NextApiRequest} from "next";
 
 export async function POST(req: NextApiRequest) {
-  const search = req.body.search;
+  console.log('SUBMITTING SEARCH');
+  const {search} = req.body;
+
+  console.log('SEARCH', search);
 }
