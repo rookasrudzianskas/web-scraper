2e206a6363177bc42a6dd7db28b21b83b37d5084
Rokas <rookas.rudzenskas@gmail.com>
2023-03-03 14:27:43 +0200

    feat: add magnifying glass icon
    This commit adds a magnifying glass icon to the home page.
    
    The icon is from the Heroicons library.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { DocumentMagnifyingGlassIcon } from '@heroicons/react/24/outline'
 
 const HomePage = ({}) => {
   return (
     <div>
-      <h1>Amazon Web Scraper</h1>
+      <div>
+        <DocumentMagnifyingGlassIcon className="h-64 w-64 text-indigo-600/20" />
+      </div>
     </div>
   );
 };
